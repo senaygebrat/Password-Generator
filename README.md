cd307586cce560d6209ffbb39c89b2a7ec56641f
Senay Gebrat <senaygebrat@gmail.com>
2022-09-28 21:31:11 -0700

    updated README.md and functions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Password-Generator
+
+# About Passsword Generator
+
+The generate button prompts the user to select the criteria for the password, confirming whether or not to include lowercase, uppercase, numberic, and/or special characters with a length of 8-128 characters.
+
+# Code Snippet
+
+# Author Links
+
+[LinkedIn](https://www.linkedin.com/in/senay-gebrat-566b78250/)
+[Github](https://github.com/senaygebrat)
--- script.js
+++ script.js
@@ -4,9 +4,12 @@ var passwordLength
 var lowCase 
 var hasLowCase
 var upCase
-VAR hasUpCase
+var hasUpCase
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var hasNum
+var hasSpecialCharacter
 var specialCharacter = [
   '[', '`', '!', '@',  '#', '$', '%',  //googled special characters array
   '^', '&', '*', '(',  ')', '_', '+',
@@ -17,59 +20,105 @@ var specialCharacter = [
 
 // Write password to the #password input
 function writePassword() {
+  checkPasswordLength()
+  checkLowCase()
+  checkUpCase()
+  checkNumeric()
+
+  //write all calls for functions here
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
   passwordText.value = password;
 }
+generateBtn.addEventListener("click", writePassword);
+
 
 function checkPasswordLength(){
   passwordLength=prompt("Password must be in between 8 and 128 characters");
-    if(passwordLength <= 8 || passwordLength >= 128){
-      alert("Password invalid, try again!");
-      checkPasswordLength();
+    if(passwordLength >= 8 && passwordLength <= 128){  //why isn't it working at 8?
+    } 
+    else(checkPasswordLength())
+
     }
-}
 
-function checkLowCase(){
-  lowCase=prompt("Would you like to use lower case letters? Press y for yes, and n for no.");
-  if(lowCase == "y"){
-     hasLowCase = true
-  }
 
-  if(lowCase == "n"){
-    hasLowCase = false
-  }
-  else{
-    alert("Invalid selection.")
-    checkLowCase();
-  }
-}
+  //  function checkLowCase(){
+  //   confirm("Do you want to include lowercase letters?");
 
-function checkUpCase(){
-  upCase=prompt("Would you like to use upper case letters? Press y for yes, and n for no.")
-  if(upCase == "y"){
-    hasUpCase = true
- }
+    function checkLowCase(){
+      LowCase=prompt("Would you like to use lower case letters? Press y for yes, and n for no.")
+      if(LowCase === "y" || LowCase === "n"){
+        hasLowCase = true
+      }
+      else(checkLowCase())
+      
+   }
+      
+    function checkUpCase(){
+      upCase=prompt("Would you like to use upper case letters? Press y for yes, and n for no.")
+      if(upCase === "y" || upCase === "n"){
+         hasUpCase = true
+      }
+      else(checkUpCase())
+    }
 
- if(upCase == "n"){
-   hasUpCase = false
- }
+    function checkNumeric(){
+       numeric=prompt("Would you like to use numbers? Press y for yes, and n for no.")
+       if(numeric === "y" || numeric === "n"){
+         hasNum = true
+       }
+       else(checkNumeric())
+    }
+
+    function checkSpecialCharacter(){
+      specialCharacter=prompt("Would you like to use special characters? Press y for yes, and n for no.")
+      if(specialCharacter === "y" || specialCharacter === "n"){
+        
+      }
+    }
+
+
+//  if(upCase == "n"){
+//    hasUpCase = false
+//  }
  
- else{
-   alert("Invalid selection.")
-   checkUpCase();
- }
-}
+//  else{
+//    alert("Invalid selection.")
+//    checkUpCase();
+//  }
+// }
 
+
+
+
+
+
+// function checkLowCase(){
+//   if (haslowcase=prompt("Would you like to use lower case letters?") == true) {
+//     hasLowCase = "Input saved";
+//   }
+  
+  
+//   lowCase=prompt("Would you like to use lower case letters? Press y for yes, and n for no.");
+//   if(lowCase == "y"){
+//      hasLowCase = true
+//   }
+
+//   if(lowCase == "n"){
+//     hasLowCase = false
+//   }
+//   else{
+//     alert("Invalid selection.")
+//     checkLowCase();
+//   }
+// }
 
 
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-checkPasswordLength()
-checkLowCase()
-checkUpCase()
+// generateBtn.addEventListener("click", writePassword);
